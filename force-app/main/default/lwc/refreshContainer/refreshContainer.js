759c3221589507c649db2da2354d4f21390a68de
mohdfaisal360 <mohd.faisal@360degreecloud.in>
2023-11-15 18:13:55 +0530

    test

--- force-app/main/default/lwc/refreshContainer/refreshContainer.js
+++ force-app/main/default/lwc/refreshContainer/refreshContainer.js
@@ -1,35 +1,4 @@
-import { LightningElement,api } from 'lwc';
-import getAccountRating from '@salesforce/apex/RefrenceController.getAccountRating'
-import {registerRefreshContainer, unregisterRefreshContainer} from 'lightning/refresh';
-
+import { LightningElement } from 'lwc';
 export default class RefreshContainer extends LightningElement {
-   @api recordId;
-   ratingValue;
-   refreshContainerID
-
-   connectedCallback(){
-      this.refreshContainerID = registerRefreshContainer(this,this.RefreshContainer) //(contextElement,providerMethod)
-      this.fetchingRating()
-      console.log('Connected>>>'+this.fetchingRating())
-   }
-   //disconnectedCallback() {
-   //   unregisterRefreshContainer(this.refreshContainerID);
-   //}
-   RefreshContainer(){
-      return new Promise(resolve=>{
-          console.log('relove>>'+resolve)
-          this.fetchingRating()
-          resolve(true)
-      })
-   }
-   
-
-   fetchingRating(){
-      getAccountRating({"accountId":this.recordId}).then(Response=>{
-         console.log('value>>>'+JSON.stringify(Response))
-         this.ratingValue = Response[0].Rating
-      }).catch(error=>{
-         console.eroor('I can Do it')
-      })
-   }
+  
 }
